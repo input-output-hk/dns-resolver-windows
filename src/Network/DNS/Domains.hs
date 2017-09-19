5fda58109f7da8b88d9d2dbbe5984e37c2d5432c
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-09-19 00:45:18 -0700

    Simplify the code with dns-3.0.0

--- src/Network/DNS/Domains.hs
+++ src/Network/DNS/Domains.hs
@@ -1,58 +1,15 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE CPP #-}
 
 module Network.DNS.Domains (
-      getDefaultDnsServer
-    , isValidIPv4Address
-    , newResolvConf
+    newResolvConf
     ) where
 
-import           Foreign.C             (CString, peekCString)
-import           Foreign.Marshal.Utils (maybePeek)
-import Data.Maybe (fromMaybe)
-import Data.IP
-import Text.Read (readMaybe)
 import Network.DNS as DNS
-
-#if !defined(mingw32_HOST_OS)
-#define POSIX
-#else
-#define WIN
-#endif
-
-
-
-#ifdef WIN
--- | Returns @nullPtr@ on failure.
-foreign import ccall "getWindowsDefDnsServer" getWindowsDefDnsServer :: IO CString
-#endif
-
-isValidIPv4Address :: String -> Bool
-isValidIPv4Address str = case readMaybe @IPv4 str of
-  Nothing -> False
-  Just _  -> True
-
--- | Gets one of the default DNS servers the current machine is using.
--- On failure (i.e. when there are no DNS servers available,
--- or when the operation is not supported by the operating system), returns @""@.
---
--- >>> getDefaultDnsServer
--- Just "8.8.8.8"
--- >>> getDefaultDnsServer
--- Nothing
-getDefaultDnsServer :: IO (Maybe String)
-#ifdef WIN
-getDefaultDnsServer = getWindowsDefDnsServer >>= maybePeek peekCString
-#else
-getDefaultDnsServer = pure (Just "8.8.4.4")
-#endif
 
 newResolvConf :: IO DNS.ResolvConf
 newResolvConf = do
-    let googlePublicDNS = "8.8.8.8"
-    dns <- fromMaybe googlePublicDNS <$> getDefaultDnsServer
-    return $ DNS.defaultResolvConf { DNS.resolvInfo    = DNS.RCHostName dns
-                                   , DNS.resolvTimeout = 10 * 1000 * 1000 -- 10sec timeout
+    -- This will work seamlessly on both Win & Posix.
+    return $ DNS.defaultResolvConf { DNS.resolvTimeout = 10 * 1000 * 1000 -- 10sec timeout
                                    , DNS.resolvRetry   = 10
                                    }
--- test/Main.hs
+++ test/Main.hs
@@ -6,20 +6,19 @@ import Data.Monoid
 import Test.Tasty.HUnit
 import Network.DNS.Domains
 import Network.DNS as DNS
+import Data.IP
 
 main :: IO ()
 main = do
   seed <- newResolvConf >>= makeResolvSeed
-  -- Print some debug info
-  putStrLn $ "\n*** DEBUG INFO ***\n"
-  defaultDNS <- getDefaultDnsServer
-  putStrLn $ "DEFAULT DNS: " <> show defaultDNS
   googIP <- withResolver seed $ \resolver -> DNS.lookup resolver "www.google.it" A
   c1     <- withResolver seed $ \resolver -> DNS.lookup resolver "cardano-node-0.aws.iohkdev.io" A
   c2     <- withResolver seed $ \resolver -> DNS.lookup resolver "cardano-node-1.aws.iohkdev.io" A
+  c3     <- withResolver seed $ \resolver -> DNS.lookup resolver "ntp.pool.org" A
   putStrLn $ "www.google.it resolved to: " <> show googIP
   putStrLn $ "cardano-node-0.aws.iohkdev.io resolved to: " <> show c1
   putStrLn $ "cardano-node-1.aws.iohkdev.io resolved to: " <> show c2
+  putStrLn $ "ntp.pool.org resolved to: " <> show c3
 
   putStrLn $ "\n*** END OF DEBUG INFO ***\n"
   -- Run the tests
@@ -30,25 +29,21 @@ tests = testGroup "Tests" [unitTests]
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
-  [ testCase "getDefaultDnsServer works" testGetDefaultDnsServer
-  , testCase "multi-value lookups work"  testLookups
+  [ testCase "multi-value lookups work"  testLookups
   , testCase "resolving IOHK DNS works"  testLookupsIOHK
   ]
-
-testGetDefaultDnsServer :: Assertion
-testGetDefaultDnsServer = do
-  res <- getDefaultDnsServer
-  case res of
-    Nothing -> fail "getDefaultDnsServer yielded Nothing."
-    Just x  -> isValidIPv4Address x @?= True
 
 testLookups :: Assertion
 testLookups = do
   seed <- newResolvConf >>= makeResolvSeed
   res  <- withResolver seed $ \resolver -> DNS.lookup resolver "www.google.it" A
+  res2 <- withResolver seed $ \resolver -> DNS.lookup resolver "ntp.pool.org" A
   case res of
     Left e    -> fail (show e)
     Right lst -> assertBool "nay" (all isValid lst)
+  case res2 of
+    Left e    -> fail (show e)
+    Right lst -> assertBool "nay" (all isValid lst && length lst >= 2)
 
 testLookupsIOHK :: Assertion
 testLookupsIOHK = do
